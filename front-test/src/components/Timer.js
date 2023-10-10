59a3f22e49113749d46038a22956edbc99df73e1
Andrzej Darowski <andrzej.dar@gmail.com>
2023-10-10 18:27:47 +0200

    task 2

--- front-test/src/components/Timer.js
+++ front-test/src/components/Timer.js
@@ -1,11 +1,13 @@
 import { HStack, Text, VStack } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
+  const date = useRef(undefined);
 
   useEffect(() => {
     const interval = setInterval(() => {
+      //as useState is async we need to base our calculation on previous value, not on one that could have changed
       setSeconds((prev) => prev + 1);
     }, 1000);
 
@@ -14,7 +16,10 @@ const Timer = () => {
     };
   }, []);
 
-  const date = new Date().toTimeString();
+  useEffect(() => {
+    //as scounds state forces rerender we need to limit assigning date value to only initial render
+    date.current = new Date().toTimeString();
+  }, []);
 
   return (
     <VStack gap="10px" align="left">
@@ -24,7 +29,7 @@ const Timer = () => {
       </HStack>
       <HStack>
         <Text fontWeight="bold">Time of entering the website:</Text>
-        <p>{date}</p>
+        <p>{date.current ? date.current : "loading..."}</p>
       </HStack>
     </VStack>
   );
